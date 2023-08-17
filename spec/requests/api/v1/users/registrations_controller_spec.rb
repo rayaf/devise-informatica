efbe67b51263933592473363445495be39b75c15
Ray Everton <ray.evaf@gmail.com>
2023-08-17 19:42:24 -0300

    registration and sessions fineshed

--- spec/requests/api/v1/users/registrations_controller_spec.rb
+++ spec/requests/api/v1/users/registrations_controller_spec.rb
@@ -1,7 +1,46 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::Users::RegistrationsControllers", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe Api::V1::Users::RegistrationsController, type: :controller do
+  include Devise::Test::ControllerHelpers
+
+  before do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+  end
+  
+  describe "POST #create" do
+    context "with valid params" do
+      let(:valid_params) do
+        {
+          user: {
+            email: "test@example.com",
+            password: "password"
+          }
+        }
+      end
+
+      it "creates a new user with valid params" do
+        post :create, params: valid_params
+        expect(response).to have_http_status(:success)
+        parsed_response = JSON.parse(response.body)
+        expect(parsed_response['id']).to_not be_nil
+      end
+    end
+
+    context "with invalid params" do
+      let(:invalid_params) do
+        {
+          user: {
+            email: "invalid@email.com"
+          }
+        }
+      end
+
+      it "returns errors with invalid params" do
+        post :create, params: invalid_params
+        expect(response).to have_http_status(401)
+        parsed_response = JSON.parse(response.body)
+        expect(parsed_response['errors']).to_not be_empty
+      end
+    end
   end
 end
--- spec/requests/api/v1/users/sessions_controller_spec.rb
+++ spec/requests/api/v1/users/sessions_controller_spec.rb
@@ -1,7 +1,45 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::Users::SessionsControllers", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe Api::V1::Users::SessionsController, type: :controller do
+  include Devise::Test::ControllerHelpers
+  
+  let(:user) { create(:user) }
+
+  before do
+    request.env["devise.mapping"] = Devise.mappings[:user]
+    request.headers["Accept"] = "application/json"
+  end
+
+  describe "POST #create" do
+    let(:valid_params) do
+      {
+        user: {
+          email: user.email,
+          password: user.password
+        }
+      }
+    end
+
+    it "creates a new session with valid params" do
+      post :create, params: valid_params
+      expect(response).to have_http_status(:success)
+      parsed_response = JSON.parse(response.body)
+      expect(parsed_response['id']).to_not be_nil
+    end
+
+    it "returns errors with invalid params" do
+      post :create, params: { user: { email: "invalid@example.com", password: "invalid" } }
+      expect(response).to have_http_status(:unauthorized)
+      parsed_response = JSON.parse(response.body)
+      expect(parsed_response['error']).to_not be_nil
+    end
+  end
+
+  describe "DELETE #destroy" do
+    it "destroys the user session" do
+      delete :destroy
+      expect(response).to have_http_status(:no_content)
+      expect(controller.user_signed_in?).to be_falsey
+    end
   end
 end
